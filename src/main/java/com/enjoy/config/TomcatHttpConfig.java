1f2012b72a815627150d202a0ecca72b3ceebbf6
bjli2018 <bjli2018@126.com>
2020-02-07 15:24:51 +0800

    2020-02-07 15:24 提交

---
+++ src/main/java/com/enjoy/config/TomcatHttpConfig.java
@@ -0,0 +1,45 @@
+package com.enjoy.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by BJLI on 2020/2/7.
+ */
+@Configuration
+public class TomcatHttpConfig {
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() { //springboot2 新变化
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+
+            @Override
+            protected void postProcessContext(Context context) {
+
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcat;
+    }
+
+    private Connector initiateHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(9000);
+        connector.setSecure(false);
+        connector.setRedirectPort(8444);
+        return connector;
+    }
+}
+
